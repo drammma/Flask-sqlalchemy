5d06ce336a0135260cc9299dc3e7ffb33551c60d
drammma <Safonova14092004@yandex.ru>
2021-03-12 14:07:31 +0400

    Новый файл

--- data/users.py
+++ data/users.py
@@ -1,12 +1,16 @@
+import datetime
 import sqlalchemy
 
+
+from flask_login import UserMixin
 from .db_session import SqlAlchemyBase
 
 
-class User(SqlAlchemyBase):
+class User(SqlAlchemyBase, UserMixin):
     __tablename__ = 'users'
 
-    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
     surname = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     age = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
